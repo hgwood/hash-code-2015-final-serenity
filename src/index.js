74a787f76b7c413454b391a5c9cded1e7fef59af
Hugo Wood <hx4.5@free.fr>
2015-03-27 15:15:31 +0100

    replaced file read/write by streams using stdin and stdout

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 "use strict";
 
-var fs = require("fs");
+var highland = require("highland");
+var toStream = require("highland-tostream-reducer");
 var solve = require("./solve");
 
-var inputFile = process.argv[2] || "./input.txt";
-var outputFile = process.argv[3] || "./output.txt";
-
-fs.readFile(inputFile, function(readFileErr, input) {
-  if (readFileErr) throw readFileErr;
-  var output = solve(input);
-  fs.writeFile(outputFile, output, function(writeFileErr) {
-    if (writeFileErr) throw writeFileErr;
-  });
-});
+highland(process.stdin)
+  .invoke("toString")
+  .reduce1(toStream(highland.add))
+  .map(solve)
+  .pipe(process.stdout);
