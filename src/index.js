dc7e918fef78318d7d1b66cc6c28bfba3940c904
Hugo Wood <hx4.5@free.fr>
2015-03-27 21:38:08 +0100

    fixed stream issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 "use strict";
 
 var highland = require("highland");
-var toStream = require("highland-tostream-reducer");
 var solve = require("solve");
 
 highland(process.stdin)
   .invoke("toString")
-  .reduce1(toStream(highland.add))
+  .reduce1(highland.add)
   .map(solve)
   .pipe(process.stdout);
