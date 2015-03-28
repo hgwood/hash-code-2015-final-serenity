7ddc0a3706323fd53287c8eeeb95234c3c5327fe
Hugo Wood <hx4.5@free.fr>
2015-03-28 11:44:27 +0100

    parser

--- src/test/test.js
+++ src/test/test.js
@@ -10,7 +10,27 @@ test("this is a dummy test", function(t) {
 });
 
 test("parse", function(t) {
-  t.deepEqual(parse(""), {});
+  t.deepEqual(parse("2 2 2\n1 1 1 2\n0 0\n1 1\n0 1 0 1\n1 0 1 0\n0 0 1 1\n1 0 0 1"), {
+    nx: 2,
+    ny: 2,
+    nz: 2,
+    ntargets: 1,
+    range: 1,
+    nloons: 1,
+    nsteps: 2,
+    home: {x: 0, y: 0},
+    targets: [{x: 1, y: 1}],
+    winds: [
+      [
+        [{y: 0, x: 1}, {y: 0, x: 1}],
+        [{y: 1, x: 0}, {y: 1, x: 0}]
+      ],
+      [
+        [{y: 0, x: 0}, {y: 1, x: 1}],
+        [{y: 1, x: 0}, {y: 0, x: 1}]
+      ]
+    ]
+  });
   t.end();
 });
 
