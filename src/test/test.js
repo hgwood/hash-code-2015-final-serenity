8d83ac5ca8c15a5140185168b9c4183745d1e59b
Hugo Wood <hx4.5@free.fr>
2015-03-28 11:44:12 +0100

    dummy tests for parse and unparse

--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,20 @@
 "use strict";
 
 var test = require("tape");
+var parse = require("parse");
+var unparse = require("unparse");
 
 test("this is a dummy test", function(t) {
   t.ok(true);
+  t.end();
+});
+
+test("parse", function(t) {
+  t.deepEqual(parse(""), {});
+  t.end();
+});
+
+test("unparse", function(t) {
+  t.equal(unparse({}), "[object Object]");
   t.end();
 });
