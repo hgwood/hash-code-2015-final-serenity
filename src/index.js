45103ae69e263a7db490ad53f63ced548c16d75c
jlandure <julien.landure@gmail.com>
2015-03-30 08:22:09 +0200

    trying mode cluster
    deleted	:   problem.tmp

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 "use strict";
 
-var fs = require("fs");
-var solve = require("solve");
+var solve = require("solve-cluster");
 
-var inputFile = process.argv[2] || "./input.txt";
-var outputFile = process.argv[3] || "./output.txt";
-
-fs.readFile(inputFile, function(readFileErr, input) {
-  if (readFileErr) throw readFileErr;
-  var output = solve(input.toString());
-  fs.writeFile(outputFile, output, function(writeFileErr) {
-    if (writeFileErr) throw writeFileErr;
-  });
-});
+solve();
